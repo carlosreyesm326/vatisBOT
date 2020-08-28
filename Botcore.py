f7e04ac18b28a756438dedff5777a01e5bf7a8be
Carlosreyesm <carlos-reyes26@hotmail.com>
2020-08-27 19:10:33 -0500

    Conexion funcional con Discord

--- Botcore.py
+++ Botcore.py
@@ -1,2 +1,11 @@
 import discord
-from discord import commands
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='>>', description = 'Bot for youtube music')
+
+@bot.command()
+async def description(ctx):
+    await ctx.send('Este bot es creado por CarlosReyesM')
+
+bot.run('NzQ4Njg5NDkyMDY3NDgzNzMx.X0hFbA.cpcwR9JW-iJ0pFxb8Q3sQbqV2uw')
